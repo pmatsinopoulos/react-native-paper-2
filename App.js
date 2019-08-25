cbbe26388e564e8ce0a2597a5e451e2525674074
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-08-25 08:19:05 +0300

    Chip demo

--- App.js
+++ App.js
@@ -7,14 +7,7 @@
  */
 
 import React, {Fragment} from 'react';
-import {
-  Appbar,
-  Avatar,
-  Button,
-  Card,
-  Title,
-  Paragraph,
-} from 'react-native-paper';
+import {Appbar, Avatar, Chip} from 'react-native-paper';
 
 const App = () => {
   return (
@@ -22,22 +15,19 @@ const App = () => {
       <Appbar.Header>
         <Appbar.Content title="Vehicle Management" subtitle="Hi!" />
       </Appbar.Header>
-      <Card>
-        <Card.Title
-          title="Card title"
-          subtitle="Card subtitle"
-          left={props => <Avatar.Icon {...props} icon="folder" />}
-        />
-        <Card.Content>
-          <Title>Card Title</Title>
-          <Paragraph>Card Content</Paragraph>
-        </Card.Content>
-        <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
-        <Card.Actions>
-          <Button>Cancel</Button>
-          <Button>Ok</Button>
-        </Card.Actions>
-      </Card>
+      <Chip
+        onClose={() => console.log('closed')}
+        selected={true}
+        avatar={
+          <Avatar.Image
+            source={require('./assets/images/lion.jpg')}
+            size={24}
+          />
+        }
+        onPress={() => console.log('Pressed')}
+        mode="outlined">
+        Example Chip
+      </Chip>
     </Fragment>
   );
 };
