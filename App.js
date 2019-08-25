5ef058191d630a5344add0f114980f5b8430adc5
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-08-25 10:07:06 +0300

    Accordion demo

--- App.js
+++ App.js
@@ -6,23 +6,33 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
-import {Appbar, IconButton, Colors} from 'react-native-paper';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import React, {Fragment, useState} from 'react';
+import {Appbar, List} from 'react-native-paper';
 
 const App = () => {
+  const [accordionExpanded, setAccordionExpanded] = useState(false);
   return (
     <Fragment>
       <Appbar.Header>
         <Appbar.Content title="Vehicle Management" subtitle="Hi!" />
       </Appbar.Header>
-      <IconButton
-        icon={({size, color}) => (
-          <MaterialCommunityIcons name="av-timer" size={size} color={color} />
-        )}
-        color={Colors.red500}
-        size={20}
-      />
+      <List.Section title="List Section Title">
+        <List.Accordion
+          title="Uncontrolled Accordion"
+          left={props => <List.Icon {...props} icon="folder" />}>
+          <List.Item title="First Item" />
+          <List.Item title="Second Item" />
+        </List.Accordion>
+
+        <List.Accordion
+          title="Controlled Accordion"
+          left={props => <List.Icon {...props} icon="folder" />}
+          onPress={() => setAccordionExpanded(prev => !prev)}
+          expanded={accordionExpanded}>
+          <List.Item title="First Item" />
+          <List.Item title="Second Item" />
+        </List.Accordion>
+      </List.Section>
     </Fragment>
   );
 };
