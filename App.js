a17c0c59070c61366ddd2c4832ab3d87850efdef
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-08-25 10:46:41 +0300

    ProgressBar demo

--- App.js
+++ App.js
@@ -6,25 +6,16 @@
  * @flow
  */
 
-import React, {Fragment, useState} from 'react';
-import {Appbar, Button, Modal, Portal, Text} from 'react-native-paper';
+import React, {Fragment} from 'react';
+import {Appbar, ProgressBar, Colors} from 'react-native-paper';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-
   return (
     <Fragment>
       <Appbar.Header>
         <Appbar.Content title="Vehicle Management" subtitle="Hi!" />
       </Appbar.Header>
-      <Portal>
-        <Modal visible={visible} onDismiss={() => setVisible(false)}>
-          <Text>Foo text in Modal</Text>
-        </Modal>
-      </Portal>
-      <Button style={{marginTop: 30}} onPress={() => setVisible(true)}>
-        Show
-      </Button>
+      <ProgressBar progress={0.5} color={Colors.red500} />
     </Fragment>
   );
 };
