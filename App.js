fe437b52c99ca9e66467c9b48e782321a3e2cfe5
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-08-25 07:34:02 +0300

    Banner below the Appbar.Header

--- App.js
+++ App.js
@@ -6,34 +6,42 @@
  * @flow
  */
 
-import React, {useState} from 'react';
+import React, {Fragment, useState} from 'react';
 import {Image} from 'react-native';
-import {Banner} from 'react-native-paper';
+import {Appbar, Banner} from 'react-native-paper';
 
 const App = () => {
   const [bannerVisible, setBannerVisible] = useState(true);
 
   return (
-    <Banner
-      visible={bannerVisible}
-      actions={[
-        {
-          label: 'Fix it',
-          onPress: () => setBannerVisible(false),
-        },
-        {
-          label: 'Learn More',
-          onPress: () => setBannerVisible(false),
-        },
-      ]}
-      image={({size}) => (
-        <Image
-          source={{uri: `https://via.placeholder.com/${size}`}}
-          style={{width: size, height: size}}
+    <Fragment>
+      <Appbar.Header>
+        <Appbar.Content
+          title="Vehicle Management"
+          subtitle="Credit Card Transaction"
         />
-      )}>
-      There was a problem processing a transaction on your credit card
-    </Banner>
+      </Appbar.Header>
+      <Banner
+        visible={bannerVisible}
+        actions={[
+          {
+            label: 'Fix it',
+            onPress: () => setBannerVisible(false),
+          },
+          {
+            label: 'Learn More',
+            onPress: () => setBannerVisible(false),
+          },
+        ]}
+        image={({size}) => (
+          <Image
+            source={{uri: `https://via.placeholder.com/${size}`}}
+            style={{width: size, height: size}}
+          />
+        )}>
+        There was a problem processing a transaction on your credit card
+      </Banner>
+    </Fragment>
   );
 };
 
